fd1d4fda0ed2fe09a0d067826b00cd1ca11bcc44
George Stavrinos <gstavrinos@iit.demokritos.gr>
2018-05-18 01:09:53 +0300

    Everything ready, I think :grinning:

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-Copy in ~/.config/sublime-text-3/Packages/User
+# Installation instructions
+`cp sublime_license_killer/die_popup_die.py ~/.config/sublime-text-3/Packages/User`
+##### Remove the cloned folder (optional)
+`rm -rf sublime_license_killer`
+
+---
+# Disclaimer
+The plugin has been tested on Ubuntu 16.04. If you are a Mac or Windows user and the plugin does not work for you, feel free to submit a pull request! :wink:
--- die_popup_die.py
+++ die_popup_die.py
@@ -1,18 +1,28 @@
+import os
+import signal
 import sublime
 import sublime_plugin
 from time import sleep
-import os, signal
+import subprocess, shlex
 
 class StopAnnoyingMe(sublime_plugin.EventListener):
     def run(self, edit):
-        w = sublime.windows()
-        for i in w:
-            print(len(i.views()))
-        #print(w)
+        pass
 
     def on_post_save_async(self, view):
-        #sleep(5)
-        w = sublime.windows()
-        print("Activated!!")
-        for i in w:
-            print(i.id())
+        command = "wmctrl -lx"
+        command = shlex.split(command)
+        p = subprocess.Popen(command, stdout=subprocess.PIPE)
+        out, err = p.communicate()
+        out = out.splitlines()
+        for w in out:
+            w = str(w)
+            # Search only for windows with the sublime WM class!
+            w = w.split()
+            if len(w) == 5 and w[2] == "sublime_text.Sublime_text" and w[4] == "'":
+                # DIE POPUP!
+                command = "wmctrl -ic " + w[0][2:]
+                command = shlex.split(command)
+                subprocess.Popen(command)
+                print(command)
+                break;
